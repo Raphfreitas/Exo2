a4e3d73fd01f6c879ee3de88bdfcd46a3dff4ecd
raphfreitas <rf98@live.fr>
2018-10-04 16:50:58 +0200

    deuxième reponse

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ for ($i=0; $i <100 ; $i++)
 
 for ($i=0; $i<=100 ; $i++) 
 { 
+   
     $y=0;
     for ($j=0; $j <100 ; $j++) 
     { 
@@ -19,7 +20,19 @@ for ($i=0; $i<=100 ; $i++)
        } 
        
     }
-    echo "La valeur " .$i." est presente ".$y;
+    echo"<span";
+    if ($y>2) 
+    {
+        ?>
+        
+        style="background-color:red; border:solid 1px black;"
+
+        <?php
+
+
+    }
+
+    echo ">La valeur " .$i." est presente ".$y." fois .</span>";
     echo"<br>";
 }
 
